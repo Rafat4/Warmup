28f18a227b1ec317fa4b2d633d604303241e060b
Rafat4 <eng1.reefo@gmail.com>
2019-03-20 09:39:05 +0200

    here is the solution of repeat st

--- strings.js
+++ strings.js
@@ -3,5 +3,9 @@
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
 	function repeatStr (n, s) {
+		var string ="";
+		for (var i =0; i <n ; i ++){
+			string =string+s;
+		} return string ;
 		//your code is here
 	}
